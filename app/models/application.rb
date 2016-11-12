f8e954abe3a307bdb07073d7476c197ad2076831
Truong Tien Manh <hacnguyet1994@gmail.com>
2016-11-12 00:04:49 +0000

    Almost done, fucking tired!

--- app/models/application.rb
+++ app/models/application.rb
@@ -6,26 +6,58 @@ class Application < ApplicationRecord
   mount_uploader :appimage, AppimageUploader
 
 	
-	def self.app_search(search,category)
+	def self.app_search(search,category = 'all')
     if search
-      if category
+      if category == 'all'
         self
           .select("applications.*,companies.company_name")
           .left_outer_joins(company_applications: :company)
           .left_outer_joins(application_categories: :category)
           .where('application_name LIKE ? OR applications.description LIKE ?', "%#{search}%", "%#{search}%")
-          .where('category_name = ? OR category_name IS NULL', "%#{search}%")
+          .order("applications.created_at DESC")
       else
         self
           .select("applications.*,companies.company_name")
           .left_outer_joins(company_applications: :company)
           .left_outer_joins(application_categories: :category)
           .where('application_name LIKE ? OR applications.description LIKE ?', "%#{search}%", "%#{search}%")
+          .where('category_name = ?', category)
+          .order("applications.created_at DESC")
       end    
     else
-      self.select("applications.*,companies.company_name").left_outer_joins(company_applications: :company)
+      self
+          .select("applications.*,companies.company_name")
+          .left_outer_joins(company_applications: :company)
+          .order("applications.created_at DESC")
     end
   end
 
+  def self.app_show(id)
+    self
+      .select("applications.*,companies.company_name")
+      .left_outer_joins(company_applications: :company)
+      .left_outer_joins(application_categories: :category)
+      .where('applications.id LIKE ?', id)
+  end
+  
+  def average_rating
+    if self.reviews.size > 0
+        self.reviews.average(:star).round(1)
+    else
+        0
+    end
+  end
+  
+  def rating_count(star)
+    self.reviews.where(star: star).size
+  end
+  
+  def rating_percent(star)
+    if self.reviews.size > 0
+      self.reviews.where(star: star).size * 100 / self.reviews.size 
+    else 
+      0
+    end  
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get '/search', to: 'search#index'
   get '/app', to: 'app#index'
+  get '/app/update', to: 'app#update'
   root "static_pages#home"
 
   namespace :admin do
