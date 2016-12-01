6d90f7a3e2bfbe8e1e39182c7ef5e31c4e1e0714
Truong Tien Manh <hacnguyet1994@gmail.com>
2016-12-01 06:15:24 +0000

    like, screenshot

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :application
+  has_many   :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :reviews
+  has_many :likes
   mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
